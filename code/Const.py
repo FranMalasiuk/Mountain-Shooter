71eee59c6258752fccaf6e0713cf7ccb545867b8
franmalasiuk <fran.malasiuk@gmail.com>
2026-03-17 17:35:38 -0300

    Creating asset folder Inserting all assets Creating the menu: backgroud, sound and fonts Creating the const file

---
+++ code/Const.py
@@ -0,0 +1,15 @@
+# C
+COLOR_PINK = (255,20,147)
+COLOR_WHITE = (255,255,255)
+COLOR_BLACK = (0,0,0)
+
+# M
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
+               'SCORE',
+               'EXIT')
+
+# W
+WIN_WIDTH = 690
+WIN_HEIGHT = 387
--- code/Menu.py
+++ code/Menu.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from hmac import new
+
 import pygame.image
+from pygame.font import Font
+from pygame.rect import Rect
+from pygame.surface import Surface
+
+from code.Const import WIN_WIDTH, COLOR_PINK, MENU_OPTION, COLOR_WHITE
 
 
 class Menu:
@@ -11,6 +18,26 @@ class Menu:
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
-        self.window.blit(source=self.surf, dest=self.rect)
-        pygame.display.flip()
-        pass
+        pygame.mixer_music.load('./asset/MenuSong.mp3')
+        pygame.mixer_music.play(-1)
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(50, "CLOUDY", COLOR_PINK, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "SHOOTER", COLOR_PINK, ((WIN_WIDTH / 2), 120))
+
+            for i in range(len(MENU_OPTION)):
+                self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
+
+            pygame.display.flip()
+
+            # Check for all events
+            for event in pygame.event.get():
+                 if event.type == pygame.QUIT:
+                     pygame.quit()  # Close Window
+                     quit()  # end pygame
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
