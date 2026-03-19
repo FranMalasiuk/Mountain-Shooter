746d73ba4d4ef70fc503a9a9618b68ca858cd2fc
franmalasiuk <fran.malasiuk@gmail.com>
2026-03-19 19:03:55 -0300

    Level 2 implementation

--- code/Const.py
+++ code/Const.py
@@ -73,10 +73,10 @@ ENTITY_SCORE = {
 }
 
 ENTITY_SHOT_DELAY = {
-    'Player1': 20,
-    'Player2': 15,
-    'Enemy1': 100,
-    'Enemy2': 200,
+    'Player1': 30,
+    'Player2': 30,
+    'Enemy1': 50,
+    'Enemy2': 50,
 }
 
 ENTITY_SPEED = {
@@ -85,16 +85,22 @@ ENTITY_SPEED = {
     'Level1Bg2': 2,
     'Level1Bg3': 3,
     'Level1Bg4': 4,
+    'Level2Bg0': 0,
+    'Level2Bg1': 1,
+    'Level2Bg2': 2,
+    'Level2Bg3': 3,
+    'Level2Bg4': 4,
     'Player1': 3,
-    'Player1Shot': 1,
+    'Player1Shot': 5,
     'Player2': 3,
-    'Player2Shot': 3,
-    'Enemy1': 1,
+    'Player2Shot': 10,
+    'Enemy1': 2,
     'Enemy1Shot': 5,
-    'Enemy2': 1,
-    'Enemy2Shot': 2,
+    'Enemy2': 2,
+    'Enemy2Shot': 5,
 }
 EVENT_ENEMY = pygame.USEREVENT + 1
+EVENT_TIMEOUT = pygame.USEREVENT + 2
 
 # M
 MENU_OPTION = ('NEW GAME 1P',
@@ -107,16 +113,20 @@ MENU_OPTION = ('NEW GAME 1P',
 PLAYER_KEY_UP = {'Player1': pygame.K_UP,
                  'Player2': pygame.K_w}
 PLAYER_KEY_DOWN = {'Player1': pygame.K_DOWN,
-                 'Player2': pygame.K_s}
+                   'Player2': pygame.K_s}
 PLAYER_KEY_LEFT = {'Player1': pygame.K_LEFT,
-                 'Player2': pygame.K_a}
+                   'Player2': pygame.K_a}
 PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT,
-                 'Player2': pygame.K_d}
+                    'Player2': pygame.K_d}
 PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
-                 'Player2': pygame.K_LCTRL}
+                    'Player2': pygame.K_LCTRL}
 
 # S
-SPAWN_TIME = 4000
+SPAWN_TIME = 2000
+
+# T
+TIMEOUT_LEVEL = 20000  # 20s
+TIMEOUT_STEP = 100  # 100ms
 
 # W
 WIN_WIDTH = 576
